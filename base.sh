c4495ed8edb27536a7c4db9459d7c026930405d7
gamingstill <himanshuj08@live.com>
2018-09-28 21:57:03 +0530

    Create base.sh

---
+++ base.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# -*- Mode: sh; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Authors:
+#   Kevin Fowlks <fowlk1kd@gmail.com>
+#
+# Description:
+#   A post base tool installation bash script for Ubuntu 16.04+
+#
+# Legal Preamble:
+#
+# This script is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 3.
+#
+# This script is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <https://www.gnu.org/licenses/gpl-3.0.txt>
+# tab width
+
+tabs 4
+clear
+
+ROOT_UID=0
+E_NOTROOT=1
+ADMIN_EMAIL=fowlk1kd@gmail.com
+DEBIAN_FRONTEND=noninteracti
